1dddec8bf63b21c2f7aad0eae36608c72fd52dc5
TuxCraft <tux.craft.42@gmail.com>
2014-01-06 13:58:12 -0500

    Added localizations

--- common/mcdelta/core/support/SupportTwilightForest.java
+++ common/mcdelta/core/support/SupportTwilightForest.java
@@ -36,7 +36,7 @@ public class SupportTwilightForest implements LimitedModSupport
      public void preInit ()
      {
           IRONWOOD = MaterialRegistry.add(new DeltaInfo("ironwood", 8944764, "ingotIronwood", false, true, false, false), new ToolInfo(2, 512, 6.5F, 2.0F, 25), null, new ExtraInfo(EnumChatFormatting.WHITE, new SimpleEntry<Enchantment, Integer>(Enchantment.knockback, 1), new SimpleEntry<Enchantment, Integer>(Enchantment.unbreaking, 1)));
-          FIERY = MaterialRegistry.add(new DeltaInfo("fiery", 3941155, "ingotFiery", false, true, false, false), new ToolInfo(4, 1024, 9.0F, 4.0F, 10), null, new ExtraInfo(EnumChatFormatting.LIGHT_PURPLE, new SimpleEntry<Enchantment, Integer>(Enchantment.fireAspect, 1), null, new SimpleEntry<String, Integer>("blazeRod", 16501540)));
+          FIERY = MaterialRegistry.add(new DeltaInfo("fiery", 3941155, "ingotFiery", false, true, false, false), new ToolInfo(4, 1024, 9.0F, 4.0F, 10), null, new ExtraInfo(EnumChatFormatting.AQUA, new SimpleEntry<Enchantment, Integer>(Enchantment.fireAspect, 1), null, new SimpleEntry<String, Integer>("blazeRod", 16501540)));
           STEELEAF = MaterialRegistry.add(new DeltaInfo("stealeaf", 7184990, "ingotSteeleaf", false, true, false, false), new ToolInfo(3, 131, 8.0F, 3.0F, 9), null, new ExtraInfo(EnumChatFormatting.YELLOW, new SimpleEntry<Enchantment, Integer>(Enchantment.looting, 2), new SimpleEntry<Enchantment, Integer>(Enchantment.efficiency, 2)));
           KNIGHTMETAL = MaterialRegistry.add(new DeltaInfo("niteMetal", 12900014, "ingotKnightMetal", false, true, false, false), new ToolInfo(3, 512, 8.0F, 3.0F, 8), null, new ExtraInfo(EnumChatFormatting.AQUA));
      }
