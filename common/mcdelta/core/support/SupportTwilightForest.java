d98426164fc5170eac4992c65aefa2c5de8dcb10
TuxCraft <tux.craft.42@gmail.com>
2014-01-05 22:50:44 -0500

    Screw mod support! That's boring, let's work on something fun :P

--- common/mcdelta/core/support/SupportTwilightForest.java
+++ common/mcdelta/core/support/SupportTwilightForest.java
@@ -34,7 +34,7 @@ public class SupportTwilightForest implements LimitedModSupport
      public void preInit ()
      {
           IRONWOOD = MaterialRegistry.add(new DeltaInfo("ironwood", 8944764, "ingotIronwood", false, true, false, false), new ToolInfo(2, 512, 6.5F, 2.0F, 25), null, new ExtraInfo(EnumChatFormatting.WHITE, new SimpleEntry<Enchantment, Integer>(Enchantment.knockback, 1), new SimpleEntry<Enchantment, Integer>(Enchantment.unbreaking, 1)));
-          FIERY = MaterialRegistry.add(new DeltaInfo("fiery", 3941155, "ingotFiery", false, true, false, false), new ToolInfo(4, 1024, 9.0F, 4.0F, 10), null, new ExtraInfo(EnumChatFormatting.WHITE, new SimpleEntry<Enchantment, Integer>(Enchantment.knockback, 1), new SimpleEntry<Enchantment, Integer>(Enchantment.unbreaking, 1)));
+          FIERY = MaterialRegistry.add(new DeltaInfo("fiery", 3941155, "ingotFiery", false, true, false, false), new ToolInfo(4, 1024, 9.0F, 4.0F, 10), null, new ExtraInfo(EnumChatFormatting.WHITE, new SimpleEntry<Enchantment, Integer>(Enchantment.fireAspect, 1), null, new SimpleEntry<String, Integer>("blazeRod", 16501540)));
      }
      
      
@@ -50,5 +50,7 @@ public class SupportTwilightForest implements LimitedModSupport
           Item fieryItem = Assets.findItemWithName("fieryIngot");
           if (ironWood != null)
                OreDictionary.registerOre("ingotFiery", fieryItem);
+          
+          OreDictionary.registerOre("blazeRod", Item.blazeRod);
      }
 }
