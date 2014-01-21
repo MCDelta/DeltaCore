95f318e143cc892a53e2095504ffd10799e36c56
TuxCraft <tux.craft.42@gmail.com>
2014-01-21 15:12:54 -0500

    Added Light Source block property

--- common/mcdelta/core/block/Properties.java
+++ common/mcdelta/core/block/Properties.java
@@ -4,7 +4,8 @@ import net.minecraft.block.Block;
 
 public enum Properties
 {
-     STONE;
+     STONE,
+     LIGHT_SOURCE;
      
      public void setProperties (Block block)
      {
@@ -13,6 +14,13 @@ public enum Properties
                case STONE:
                     block.setHardness(2.0F);
                     block.setResistance(10.0F);
+                    block.setStepSound(Block.soundStoneFootstep);
+                    break;
+               case LIGHT_SOURCE:
+                    block.setHardness(0.3F);
+                    block.setLightValue(1.0F);
+                    block.setStepSound(Block.soundGlassFootstep);
+                    block.setLightValue(1.0F);
                     break;
                default:
                     break;
